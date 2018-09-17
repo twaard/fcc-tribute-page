4f45296b27e2c540e49d17913ca61c7426b9c97a
Tom Waardenburg <tom@waard.net>
2018-09-18 11:24:36 +1200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+const port = 3000;
+const path = require('path');
+
+app.use(express.static(__dirname + '/static'))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname + '/public/index.html')))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
