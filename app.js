c8dfaebb6c43bb3ddd420e787371f4729d69c6e9
Tom Waardenburg <tom@waard.net>
2018-09-18 11:34:13 +1200

    changed port variable to be heroku friendly

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 const path = require('path');
 
 app.use(express.static(__dirname + '/static'))
